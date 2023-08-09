e9b59c70687b6accec84c4e6030e4534bc480b67
Enzo Vieira <enzovieira16@hotmail.com>
2023-08-09 16:23:46 -0300

    add: Header

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Header } from "./components/Header"
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <p>Click on the Vite and React logos to learn more</p>
+    </div>
+  )
+}
+
+export default App
