c831340013299d4a21811bd601826fa5dc165939
Enzo Vieira <enzovieira16@hotmail.com>
2023-08-09 16:53:33 -0300

    add: Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import { Header } from "./components/Header"
+import { Sidebar } from "./components/Sidebar"
+
+import styles from "./App.module.css"
 
 function App() {
   return (
     <div>
       <Header />
-      <p>Click on the Vite and React logos to learn more</p>
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus
+            ea cupiditate officia modi! Ad sed, consequatur perferendis est
+            fugiat voluptate sequi doloremque libero nostrum laborum quos esse
+            quo totam et.
+          </p>
+
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus
+            ea cupiditate officia modi! Ad sed, consequatur perferendis est
+            fugiat voluptate sequi doloremque libero nostrum laborum quos esse
+            quo totam et.
+          </p>
+        </main>
+      </div>
     </div>
   )
 }
